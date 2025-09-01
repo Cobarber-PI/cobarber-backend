df14f3ad31058e9a46233cea2b1b8e127124e824
Henrique0330 <henriquebelboni@gmail.com>
2025-09-01 08:35:50 -0300

    feat: removendo os validates q não são necessarios

--- core/serializers/user.py
+++ core/serializers/user.py
@@ -16,18 +16,7 @@ class ClienteSerializer(ModelSerializer):
         fields = ['id', 'name', 'email', 'password', 'cellphone', 'DOB', 'cpf']
         extra_kwargs = {'password': {'write_only': True}}
 
-    # Valida se o email é único
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Este email já está em uso.")
-        return value
 
-    # Valida se o CPF é único
-    def validate_cpf(self, value):
-        cpf_normalizado = value.replace('.', '').replace('-', '')
-        if User.objects.filter(cpf=cpf_normalizado).exists():
-            raise serializers.ValidationError("Este CPF já está em uso.")
-        return cpf_normalizado
 
     # Valida idade mínima de 16 anos
     def validate_DOB(self, value):
@@ -37,20 +26,8 @@ class ClienteSerializer(ModelSerializer):
             idade -= 1
         if idade < 16:
             raise serializers.ValidationError("O usuário deve ter pelo menos 16 anos.")
-        return value
+        return value    
 
-    # Valida se o celular é único
-    def validate_cellphone(self, value):
-        celular_normalizado = value.replace('(', '').replace(')', '').replace('-', '').replace(' ', '')
-        if User.objects.filter(cellphone=celular_normalizado).exists():
-            raise serializers.ValidationError("Este número de celular já está em uso.")
-        return celular_normalizado
-
-    # Valida tamanho mínimo da senha
-    def validate_password(self, value):
-        if len(value) < 8:
-            raise serializers.ValidationError("A senha deve ter pelo menos 8 caracteres.")
-        return value
 
     # Criação do usuário
     def create(self, validated_data):
